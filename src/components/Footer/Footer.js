9cb1e07c77b44810295048e8cd910d4a7945d834
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-09-08 14:34:17 +0200

    Added test environment variables before deploying

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/Plant-id/Plant-id-API/wiki">Go to Docs</a>
+      <a href="https://github.com/Plant-id/Plant-id-API/wiki">Go to Plant ID Docs</a>
+      <p>Environment:  {process.env.NODE_ENV}</p>
+      <p>{process.env.REACT_APP_TEST}</p>
     </footer>
   )
 }
