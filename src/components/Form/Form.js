6c62c823e41db1024337545b55e1305d1b6aa0a2
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-09-09 14:11:37 +0200

    Fixed error in Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 const Form = () => {
   const [images, setImages] = useState([]);
-  const [searchResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState(null)
 
   const sendIdentification = () => {
     if (images) {
@@ -49,7 +49,6 @@ const Form = () => {
         setSearchResults(data);
         console.log('Success:', data);
       })
-      // .then(data => setSearchResults(data))
       .catch((error) => {
         console.error('Error:', error);
       });
@@ -57,7 +56,7 @@ const Form = () => {
   };
 
   useEffect(() => {
-    console.log(searchResults);
+    console.log('the search results', searchResults);
   }, [searchResults])
 
   return (
@@ -69,99 +68,15 @@ const Form = () => {
         <button type="button" onClick={sendIdentification}>OK</button>
       </form>
       {searchResults && <p>There are some results</p>}
-      {/* {searchResults && <p>{searchResults.suggestions[0].plant_name}</p>} */}
+      {searchResults && searchResults.suggestions.map(result => <div>
+        <p>{result.plant_name}</p>
+        <p>{result.plant_details.wiki_description.value}</p>
+        <a href={result.plant_details.wiki_description.value} target="_blank" rel="noreferrer"><p>Wiki</p></a>
+        <p>Images</p>
+        {result.similar_images.map(image => <p>{image.url}</p>)}
+      </div>)}
     </div>
   )
 }
 
 export default Form
-
-
-// import React, { useState, useEffect } from 'react'
-
-// const Form = () => {
-//   const [imageFile, setImageFile] = useState(undefined);
-//   const [imageBase64, setImageBase64] = useState('');
-
-//   // const API_KEY = process.env.REACT_APP_API_KEY;
-
-//   const fetchData = () => {
-//     console.log('In fetch data');
-//     // console.log('line 9 API KEY', API_KEY);
-//     const data = {
-//       api_key: process.env.REACT_APP_API_KEY,
-//       images: imageBase64,
-//       modifiers: ["crops_fast", "similar_images"],
-//       plant_language: "en",
-//       plant_details: ["common_names",
-//                         "url",
-//                         "name_authority",
-//                         "wiki_description",
-//                         "taxonomy",
-//                         "synonyms"]
-//     };
-
-//     if (imageBase64) {
-//       console.log('line 24', data);
-//       fetch('https://api.plant.id/v2/identify', {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify(data),
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//           console.log('Success:', data);
-//         })
-//         .catch((error) => {
-//           console.error('Error:', error);
-//         });
-//       }
-//   }
-
-//   useEffect(() => {
-//     fetchData();
-//   }, [imageBase64]);
-
-//   const convertImageToBase64 = async (e) => {
-//     e.preventDefault();
-//     console.log('line 50', imageFile);
-//     const reader = await new FileReader();
-//     reader.readAsDataURL(imageFile);
-//     reader.onload = () => {
-//       // console.log('line 12', reader.result);
-//       setImageBase64(reader.result);
-//     };
-//     reader.onerror = (error) => {
-//       console.log('line 58 ERROR:', error)
-//     };
-//    fetchData(); 
-//   }
-
-//   const saveImageToState = async (e) => {
-//     console.log('line 64', e.target.files[0]);
-//     const image = e.target.files[0];
-//     setImageFile(image);
-//   };
-
-//   return (
-//     <section>
-//       <h2>Form section</h2>
-//       {/* Added aria label below for testing */}
-//       <form aria-label="form">
-//         <input
-//           type="file"
-//           onChange={saveImageToState}
-//           required 
-//         />
-//         <button onClick={convertImageToBase64}>Submit</button>
-//       </form>
-//       {imageFile && <p>There's an image called {imageFile.name}</p>}
-//       {imageBase64 && <p>There should be string base64 for the image now as well</p>}
-//     </section>
-//   )
-// }
-
-// export default Form
-
