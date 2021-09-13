ee95671bb2141179681f4bfd50ce86cf5e117804
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-09-13 13:06:57 +0200

    Tweaked footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,6 @@ const Footer = () => {
     <footer>
       <a href="https://github.com/Plant-id/Plant-id-API/wiki">Go to Plant ID Docs</a>
       <p>Environment:  {process.env.NODE_ENV}</p>
-      <p>Testing the .env: {process.env.REACT_APP_TEST}</p>
     </footer>
   )
 }
